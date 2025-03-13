2129393f81cbb1de8cadc1f991c06234c4dd910d
Pythonesa <pythonesamar@gmail.com>
2025-03-13 18:56:24 -0300

    :tada: this starts here...

---
+++ README.md
@@ -0,0 +1,12 @@
+# Weather Shop
+
+¿Quién no necesita un carrito de compras para poder comprar su clima perfecto?
+
+## Tecnologías
+
+- Vite
+- React
+
+## Imágenes
+
+Todas las imágenes utilizadas en el proyecto son de [Icons8](https://iconos8.es/)
---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import WSLogo from '/logo.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <img src={WSLogo} className="logo" alt="Weather Shop logo" />
+        <h1>Weather Shop</h1>
+      </div>
+      <p>
+        Próximamente la tienda para poder comprar su clima perfecto
+      </p>
+    </>
+  )
+}
+
+export default App
